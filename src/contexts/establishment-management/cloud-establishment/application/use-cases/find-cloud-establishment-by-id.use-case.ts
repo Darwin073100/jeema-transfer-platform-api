b6c70ada79d1858e0d4ba41c3c150539148864bb
Edwin Garcia Quiterio <edwinegq@outlook.com>
2026-05-13 15:42:30 -0600

    FEATURE: Buscar establecimiento por id.

--- src/contexts/establishment-management/cloud-establishment/application/use-cases/find-cloud-establishment-by-id.use-case.ts
+++ src/contexts/establishment-management/cloud-establishment/application/use-cases/find-cloud-establishment-by-id.use-case.ts
@@ -1,3 +1,4 @@
+import { DNotFoundException } from "src/shared/domain/exceptions/basics/d-not-found.exception";
 import { CloudEstablishmentEntity } from "../../domain/entities/cloud-establishment.entity";
 import { CloudEstablishmentRepository } from "../../domain/repositories/cloud-establishment.repository";
 
@@ -6,8 +7,11 @@ export class FindCloudEstablishmentByIdUseCase {
     private readonly cloudEstablishmentRepository: CloudEstablishmentRepository,
   ) {}
 
-  public async execute(id: bigint): Promise<CloudEstablishmentEntity | null> {
+  public async execute(id: bigint): Promise<CloudEstablishmentEntity> {
     const establishment = await this.cloudEstablishmentRepository.findById(id);
+    if(!establishment){
+      throw new DNotFoundException('No se encontró el establecimiento.');
+    }
     return establishment;
   }
 }
--- src/contexts/establishment-management/cloud-establishment/presentation/controllers/cloud-establishment.controller.ts
+++ src/contexts/establishment-management/cloud-establishment/presentation/controllers/cloud-establishment.controller.ts
@@ -1,20 +1,40 @@
-import { BadRequestException, Body, Controller, HttpCode, HttpStatus, NotFoundException, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Param, Post } from "@nestjs/common";
 import { RegisterCloudEstablishmentUseCase } from "../../application/use-cases/register-cloud-establishment.use-case";
 import { RegisterCloudEstablishmentCommand } from "../commands/register-cloud-establishment.command";
 import { CloudEstablishmentHttpMapper } from "../mappers/cloud-establishment.http-mapper";
 import { DNotFoundException } from "src/shared/domain/exceptions/basics/d-not-found.exception";
 import { DAlreadyExistException } from "src/shared/domain/exceptions/basics/d-already-exist.exception";
+import { FindCloudEstablishmentByIdUseCase } from "../../application/use-cases/find-cloud-establishment-by-id.use-case";
+import { ParseBigIntPipe } from "src/shared/pipes/parse-bigint.pipe";
 
 @Controller('cloud-establishments')
 export class CloudEstablishmentController {
     constructor(
-        private readonly registerCloudEstablishmentUseCase: RegisterCloudEstablishmentUseCase
+        private readonly registerCloudEstablishmentUseCase: RegisterCloudEstablishmentUseCase,
+        private readonly findCloudEstablishmentByIdUseCase: FindCloudEstablishmentByIdUseCase,
     ){}
     @Post()
     @HttpCode(HttpStatus.CREATED)
     async registerCloudEstablishment(@Body() command: RegisterCloudEstablishmentCommand){
         try {
             const result = await this.registerCloudEstablishmentUseCase.execute(command);
+            return CloudEstablishmentHttpMapper.toHttpREsponse(result);
+        } catch (error) {
+            if(error instanceof DNotFoundException){
+                throw new NotFoundException(error.message);
+            }
+            if(error instanceof DAlreadyExistException){
+                throw new BadRequestException(error.message);
+            }
+            throw error;
+        }
+    }
+
+    @Get(':cloudEstablishmentId')
+    @HttpCode(HttpStatus.OK)
+    async findCloudEstablishmentById(@Param('cloudEstablishmentId', ParseBigIntPipe ) cloudEstablishmentId: bigint){
+        try {
+            const result = await this.findCloudEstablishmentByIdUseCase.execute(cloudEstablishmentId);
             return CloudEstablishmentHttpMapper.toHttpREsponse(result);
         } catch (error) {
             if(error instanceof DNotFoundException){
